5e17c63607d478b1bf9f9ff5d980485762cd262c
AliGooze <eagle110.ali@gmail.com>
2019-06-15 05:35:45 +0430

    first commit

---
+++ Assets/Scripts/Table.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table : MonoBehaviour
+{
+	public Letter[,] letter;
+	public int rows, columns;
+	public GameObject letterPrefab;
+
+
+	public void MakeLetter(int position = -1)
+	{
+		System.Random r = new System.Random();
+		if (position != -1)
+		{
+			r.Next(0, 8);
+		}
+	}
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		letter = new Letter[rows,columns];
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < columns; j++)
+			{
+				letter[i, j] = Instantiate(letterPrefab).GetComponent<Letter>();
+				letter[i, j].transform.position = new Vector3(j * 135 + 67.5f,i *135 + 67.5f + 200f, 0.0f);
+				letter[i, j].id = i;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
